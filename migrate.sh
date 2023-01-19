26787f2b50d2749ff7578728759fd9c8d65a7389
XKa-MSF-OCB <Xavier.Kaufmann@brussels.msf.org>
2023-01-19 08:21:59 +0100

    migrate.sh: amend init checks (esp. try to detect if folder "/etc/nixos/* already migrated to 1-repo config), various minor changes

--- migrate.sh
+++ migrate.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+# To migrate a NixOS Linux system installed with the old 2-repo MSF-OCB NixOS configuration to the new 1-repo one,
+# follow latest instructions at <https://github.com/MSF-OCB/NixOS-OCB/wiki/> (private repo).
+# Typically this script is executed with a command-line like this (sudo access required):
+# $ curl -L https://github.com/MSF-OCB/NixOS-OCB-install/raw/main/migrate.sh | sudo bash -s --
+
 # Set up the shell environment
 set -euo pipefail
 shopt -s extglob globstar nullglob
 
 declare -r script_name="migrate.sh"
 # TODO: keep script version string up-to-date
-declare -r script_version="v2023.01.17.0-ALPHA0"
-declare -r script_title="MSF-OCB custom NixOS Linux config 2-repo to 1-repo migration script"
+declare -r script_version="v2023.01.18.0-ALPHA0"
+declare -r script_title="MSF-OCB custom NixOS Linux configuration 2-repo to 1-repo migration script (2023-01)"
 
 ##########
 ### FUNCTIONS:
@@ -45,42 +50,47 @@ declare -r hostname="$(hostname)"
 declare -r nixos_cfg_dir="/etc/nixos"
 declare -r nixos_cfg_2repo_dir="${nixos_cfg_dir}.2repo"
 declare -r nixos_cfg_1repo_dir="${nixos_cfg_dir}.1repo"
+declare -r nixos_cfg_git_cfg_file="${nixos_cfg_dir}/.git/config"
 declare -r org_tunnel_priv_key_file="${nixos_cfg_dir}/local/id_tunnel"
-
-if [[ ! -d "${nixos_cfg_dir}" ]]; then
-  echo_err "standard NixOS config directory \"${nixos_cfg_dir}\" does NOT exist!"
-  exit 102
-fi
-
-if [[ ! -r "${org_tunnel_priv_key_file}" ]]; then
-  echo_err "organisation tunnel private key file \"${org_tunnel_priv_key_file}\" does NOT exist or is NOT readable by the current user!"
-  exit 103
-fi
-
-if [[ -d "${nixos_cfg_2repo_dir}" ]]; then
-  echo_err "old 2-repo NixOS config directory \"${nixos_cfg_2repo_dir}\" already exists!"
-  exit 104
-fi
-
-if [[ -d "${nixos_cfg_1repo_dir}" ]]; then
-  echo_err "new 1-repo NixOS config directory \"${nixos_cfg_1repo_dir}\" already exists!"
-  exit 105
-fi
 
 # Define constants for use of MSF-OCB GitHub repositories
 declare -r github_org_name="MSF-OCB"
 declare -r main_repo_name="NixOS-OCB"
 declare -r main_repo="git@github.com:${github_org_name}/${main_repo_name}.git"
 declare -r main_repo_branch="main"
-declare -r github_nixos_robot_name="OCB NixOS Robot"
-declare -r github_nixos_robot_email="69807852+nixos-ocb@users.noreply.github.com"
+# declare -r github_nixos_robot_name="OCB NixOS Robot"
+# declare -r github_nixos_robot_email="69807852+nixos-ocb@users.noreply.github.com"
+
+echo
+echo_info "checking current NixOS configuration directory \"${nixos_cfg_dir}\"..."
+if [[ ! -d "${nixos_cfg_dir}" ]]; then
+  echo_err "standard NixOS configuration directory \"${nixos_cfg_dir}\" not found!"
+  exit 103
+fi
+if [[ -f "${nixos_cfg_git_cfg_file}" ]] && grep -F "${main_repo}" "${nixos_cfg_git_cfg_file}"; then
+  echo_err "current NixOS configuration directory \"${nixos_cfg_dir}\" seems to be already migrated to the new 1-repo MSF-OCB NixOS configuration!"
+  exit 104
+fi
+if [[ ! -r "${org_tunnel_priv_key_file}" ]]; then
+  echo_err "organisation tunnel private key file \"${org_tunnel_priv_key_file}\" not found/readable by the current user!"
+  exit 105
+fi
+echo
+
+if [[ -d "${nixos_cfg_2repo_dir}" ]]; then
+  echo_err "old 2-repo NixOS configuration backup directory \"${nixos_cfg_2repo_dir}\" already exists!"
+  echo "Please check its contents to see if it is (still) needed (esp. to be able revert to the old 2-repo configuration)."
+  echo "Please only delete it if confirmed that it is not needed - if in doubt please rather rename it (better safe than sorry)."
+  echo "Then try to run again this migration script."
+  exit 106
+fi
 
 echo_info "parameters:"
 echo "- GitHub.com private repository (@branch): \"${github_org_name}/${main_repo_name}@${main_repo_branch}\""
 echo
 
 # If command 'git' is not available, try to get it via its Nix package and add its folder to the system path
-if ! which "git" >&/dev/null; then
+if ! type -p "git" >&/dev/null; then
   echo_info "downloading missing required software package 'git'..."
   PATH="$(nix-build --no-out-link -E '(import <nixpkgs> {})' -A 'git')/bin:${PATH}"
   export PATH
@@ -88,7 +98,7 @@ if ! which "git" >&/dev/null; then
   echo
 fi
 
-echo_info "about to start migrating to one MSF-OCB NixOS repo for host \"${hostname}\" on $(date +'%F_%T%z')..."
+echo_info "about to start the migration of host \"${hostname}\" to the new 1-repo MSF-OCB NixOS configuration on $(date +'%F_%T%z')..."
 echo "(Press [Ctrl+C] *now* to abort)"
 echo -ne "\n--> countdown before proceeding: "
 for countdown in $(seq 9 -1 0); do
@@ -97,15 +107,23 @@ for countdown in $(seq 9 -1 0); do
 done
 echo "GO!"
 
+if [[ -d "${nixos_cfg_1repo_dir}" ]]; then
+  echo_info "temporary new 1-repo NixOS configuration directory \"${nixos_cfg_1repo_dir}\" already exists - first purging it..."
+  rm --recursive --dir --force --preserve-root "${nixos_cfg_1repo_dir}"
+  echo
+fi
+
 echo
-echo_info "downloading MSF-OCB NixOS configuration files into \"${nixos_cfg_1repo_dir}\"..."
+echo_info "downloading new 1-repo MSF-OCB NixOS configuration files into \"${nixos_cfg_1repo_dir}\"..."
 mkdir --parents --verbose -- "${nixos_cfg_1repo_dir}"
-chmod --preserve-root --changes --reference="${nixos_cfg_dir}" -- "${nixos_cfg_1repo_dir}"
-chown --preserve-root --changes --reference="${nixos_cfg_dir}" -- "${nixos_cfg_1repo_dir}"
+chmod --reference="${nixos_cfg_dir}" --preserve-root --changes -- "${nixos_cfg_1repo_dir}"
+chown --reference="${nixos_cfg_dir}" --preserve-root --changes -- "${nixos_cfg_1repo_dir}"
 git -c core.sshCommand="ssh -F none -o IdentitiesOnly=yes -i '${org_tunnel_priv_key_file}'" clone --filter=blob:none --single-branch --branch "${main_repo_branch}" "${main_repo}" "${nixos_cfg_1repo_dir}"
+cp --archive --force --verbose -- "${nixos_cfg_dir}/local" "${nixos_cfg_1repo_dir}/"
 echo
 
-cp --archive --force --verbose -- "${nixos_cfg_dir}/local" "${nixos_cfg_1repo_dir}/"
+echo
+echo_info "switching directory \"${nixos_cfg_dir}\" to new 1-repo MSF-OCB NixOS configuration files..."
 mv --verbose -- "${nixos_cfg_dir}" "${nixos_cfg_2repo_dir}"
 mv --verbose -- "${nixos_cfg_1repo_dir}" "${nixos_cfg_dir}"
 
@@ -122,7 +140,7 @@ echo_info "rebuilding the configuration of this NixOS system..."
 nixos-rebuild switch
 
 echo
-echo_info "migrating to one MSF-OCB NixOS repo completed successfully for host \"${hostname}\" on $(date +'%F_%T%z')."
+echo_info "completed successfully the migration of host \"${hostname}\" to the new 1-repo MSF-OCB NixOS configuration on $(date +'%F_%T%z')..."
 echo
 
 ### EOF
