c3034dcd60db9fcbb4873895c2c5725e476ffbe1
XKa-MSF-OCB <Xavier.Kaufmann@brussels.msf.org>
2023-12-18 10:07:00 +0100

    `install.sh` : updated script version: v2023.12.18.0

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@
 
   declare -r script_name="install.sh"
   # TODO: keep script version string up-to-date
-  declare -r script_version="v2023.07.31.0.BETA2"
+  declare -r script_version="v2023.12.18.0"
   declare -r script_title="MSF-OCB customised NixOS Linux installation script (unified repo + flakes)"
 
   ##########
