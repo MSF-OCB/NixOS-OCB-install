e2ef07925b35672f8e0197b3d4b9545e36d7b184
Ibrahim Yerima Balla <113344794+ibrahimballamsf@users.noreply.github.com>
2023-12-18 10:08:37 +0100

    Fix Install Script (#10)
    * Fix Install Script
    
    * `install.sh` : updated script version: v2023.12.18.0
    
    ---------
    
    Co-authored-by: XKa-MSF-OCB <Xavier.Kaufmann@brussels.msf.org>

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@
 
   declare -r script_name="install.sh"
   # TODO: keep script version string up-to-date
-  declare -r script_version="v2023.07.31.0.BETA2"
+  declare -r script_version="v2023.12.18.0"
   declare -r script_title="MSF-OCB customised NixOS Linux installation script (unified repo + flakes)"
 
   ##########
@@ -749,7 +749,7 @@ EOF_sfdisk_01
       --no-root-passwd \
       --max-jobs 4 \
       --option extra-experimental-features 'flakes nix-command' \
-      --flake "${main_repo_flake}#${target_hostname}-install"
+      --flake "${main_repo_flake}#${target_hostname}"
   else
     echo
     echo_info "rebuilding the configuration of this pre-installed NixOS system..."
